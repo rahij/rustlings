fd8284dc671e161d02853e17b0936ac4c7be4760
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2023-11-11 11:34:05 +0000

    structs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i64) -> i64 {
+    let price: i64;
+    if quantity <= 40 {
+        price = 2;
+    } else {
+        price = 1;
+    }
+    return quantity * price;
+}
 
 // Don't modify this function!
 #[test]
